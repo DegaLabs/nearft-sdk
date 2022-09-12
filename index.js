f9c3def2560a91b40b885ae4bf063d3442f0607c
Do Minh Khai <khaihkd@gmail.com>
2022-09-12 19:13:21 +0100

    add get buy/sell info

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const BigNumber = require('bignumber.js')
 const nearAccount = require("./nearAccount")
 const listNFT = require('./utils/listNft')
 const HELP = {
@@ -19,7 +20,57 @@ const HELP = {
         }
         return ret
     },
-    getNFTData: listNFT.getNFTList
+    getNFTData: listNFT.getNFTList,
+    getBuyInfo: async (networkId, contractId, poolId, numItems) => {
+        const readAccount = await nearAccount.getReadOnlyAccount(networkId, contractId)
+        const poolInfo = await readAccount.viewFunction({
+            contractId: contractId,
+            methodName: "get_pool_info",
+            args: {
+                pool_id: poolId,
+            }
+        })
+        const buyInfo = await readAccount.viewFunction({
+            contractId: contractId,
+            methodName: "get_buy_info",
+            args: {
+                pool_id: poolId,
+                num_items: numItems
+            }
+        })
+        buyInfo.spot_price = poolInfo.spot_price
+        buyInfo.price_impact = new BigNumber(buyInfo.new_spot_price)
+          .minus(poolInfo.spot_price).div(poolInfo.spot_price).multipliedBy(100).toNumber()
+        if (poolInfo.pool_token_ids.length < numItems) {
+            buyInfo.error_code = 'error'
+        }
+        return buyInfo
+    },
+    getSellInfo: async (networkId, contractId, poolId, numItems) => {
+        const readAccount = await nearAccount.getReadOnlyAccount(networkId, contractId)
+        const poolInfo = await readAccount.viewFunction({
+            contractId: contractId,
+            methodName: "get_pool_info",
+            args: {
+                pool_id: poolId,
+            }
+        })
+        const buyInfo = await readAccount.viewFunction({
+            contractId: contractId,
+            methodName: "get_sell_info",
+            args: {
+                pool_id: poolId,
+                num_items: numItems
+            }
+        })
+        buyInfo.spot_price = poolInfo.spot_price
+        buyInfo.price_impact = new BigNumber(poolInfo.spot_price)
+          .minus(buyInfo.new_spot_price).div(poolInfo.spot_price).multipliedBy(100).toNumber()
+        if (poolInfo.pool_token_ids.length < numItems) {
+            buyInfo.error_code = 'error'
+        }
+        return buyInfo
+    }
 }
 
 module.exports = HELP

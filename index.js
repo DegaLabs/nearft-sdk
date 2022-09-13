dfb84d4bea42505f665779ec8a87e214561cdf61
ben <ben@degalabs.fi>
2022-09-13 14:05:26 +0100

    add buy and sell nft function

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const SDK = {
             args: {
             }
         })
-        for(var i = 0; i < ret.length; i++) {
+        for (var i = 0; i < ret.length; i++) {
             const nftOfPool = await listNFT.getOwnedNFTMetadata(networkId, ret[i].nft_token, contractId)
             nftMetadata = {}
-            for(const e of nftOfPool) {
+            for (const e of nftOfPool) {
                 nftMetadata[e.tokenId] = e
             }
             ret[i].poolTokenMetadata = nftMetadata
@@ -21,7 +21,11 @@ const SDK = {
         return ret
     },
     getNFTData: listNFT.getNFTList,
-    getBuyInfo: async (networkId, contractId, poolId, numItems) => {
+    getBuyInfo: async ({ networkId, contractId, poolId, numItems, pools }) => {
+        if (poolId === undefined) {
+            const pool = pools.find(e => e.nft_token == nftContractId)
+            poolId = pool.pool_id
+        }
         const readAccount = await nearAccount.getReadOnlyAccount(networkId, contractId)
         const poolInfo = await readAccount.viewFunction({
             contractId: contractId,
@@ -40,13 +44,17 @@ const SDK = {
         })
         buyInfo.spot_price = poolInfo.spot_price
         buyInfo.price_impact = new BigNumber(buyInfo.new_spot_price)
-          .minus(poolInfo.spot_price).div(poolInfo.spot_price).multipliedBy(100).toNumber()
+            .minus(poolInfo.spot_price).div(poolInfo.spot_price).multipliedBy(100).toNumber()
         if (poolInfo.pool_token_ids.length < numItems) {
             buyInfo.error_code = 'error'
         }
         return buyInfo
     },
-    getSellInfo: async (networkId, contractId, poolId, numItems) => {
+    getSellInfo: async ({ networkId, contractId, poolId, numItems, pools }) => {
+        if (poolId === undefined) {
+            const pool = pools.find(e => e.nft_token == nftContractId)
+            poolId = pool.pool_id
+        }
         const readAccount = await nearAccount.getReadOnlyAccount(networkId, contractId)
         const poolInfo = await readAccount.viewFunction({
             contractId: contractId,
@@ -65,11 +73,95 @@ const SDK = {
         })
         buyInfo.spot_price = poolInfo.spot_price
         buyInfo.price_impact = new BigNumber(poolInfo.spot_price)
-          .minus(buyInfo.new_spot_price).div(poolInfo.spot_price).multipliedBy(100).toNumber()
+            .minus(buyInfo.new_spot_price).div(poolInfo.spot_price).multipliedBy(100).toNumber()
         if (poolInfo.pool_token_ids.length < numItems) {
             buyInfo.error_code = 'error'
         }
         return buyInfo
+    },
+    buyNFT: async (networkId, ammContractId, pools, nftContractId, tokenIds, slippage, walletSelector, accountId) => {
+        const pool = pools.find(e => e.nft_token == nftContractId)
+        if (!pool) {
+            throw "No NFT to buy"
+        }
+        if (!tokenIds || tokenIds.length == 0) {
+            throw "Invalid output token Ids"
+        }
+        const buyInfo = await SDK.getBuyInfo({ networkId, contractId: ammContractId, poolId: pool.pool_id, numItems: tokenIds.length, pools })
+        let inputValue = buyInfo.input_value
+        inputValue = new BigNumber(inputValue).multipliedBy(100 + Math.floor(slippage * 100)).dividedBy(100).toString()
+        const wallet = await walletSelector.wallet()
+        return wallet.signAndSendTransaction({
+            signerId: accountId,
+            receiverId: ammContractId,
+            actions: [
+                {
+                    type: "FunctionCall",
+                    params: {
+                        methodName: "swap",
+                        args: {
+                            actions: [
+                                {
+                                    pool_id: pool.pool_id,
+                                    swap_type: 1,
+                                    output_token_ids: tokenIds,
+                                    num_out_nfts: tokenIds.length
+                                }
+                            ]
+                        },
+                        gas: 300000000000000,
+                        deposit: inputValue,
+                    },
+                },
+            ],
+        })
+            .catch((err) => {
+                console.log("Failed to swap");
+
+                throw err;
+            });
+    },
+    sellNFT: async (networkId, ammContractId, pools, nftContractId, tokenIds, slippage, walletSelector, accountId) => {
+        const pool = pools.find(e => e.nft_token == nftContractId)
+        if (!pool) {
+            throw "No pool to sell"
+        }
+        if (!tokenIds || tokenIds.length == 0) {
+            throw "Invalid output token Ids"
+        }
+        const sellInfo = await SDK.getSellInfo({ networkId, contractId: ammContractId, poolId: pool.pool_id, numItems: tokenIds.length, pools })
+        let outputValue = sellInfo.output_value
+        outputValue = new BigNumber(inputValue).multipliedBy(100 - Math.floor(slippage * 100)).dividedBy(100).toString()
+        const wallet = await walletSelector.wallet()
+        return wallet.signAndSendTransaction({
+            signerId: accountId,
+            receiverId: ammContractId,
+            actions: [
+                {
+                    type: "FunctionCall",
+                    params: {
+                        methodName: "swap",
+                        args: {
+                            actions: [
+                                {
+                                    pool_id: pool.pool_id,
+                                    swap_type: 0,
+                                    min_output_near: outputValue,
+                                    input_token_ids: tokenIds
+                                }
+                            ]
+                        },
+                        gas: 300000000000000,
+                        deposit: "100000000000000000000000",
+                    },
+                },
+            ],
+        })
+            .catch((err) => {
+                console.log("Failed to swap");
+
+                throw err;
+            });
     }
 }
 

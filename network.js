807fb81a7f68c6574c6c4f0248cc4fce73f52210
Do Minh Khai <khaihkd@gmail.com>
2022-10-12 15:54:00 +0700

    update node url

--- network.js
+++ network.js
@@ -4,7 +4,8 @@ module.exports = {
         nearBlocksLink: "https://testnet.nearblocks.io/",
         config: {
             networkId: 'testnet',
-            nodeUrl: 'https://rpctestnet.nearfi.finance',
+            // nodeUrl: 'https://rpctestnet.nearfi.finance',
+            nodeUrl: 'https://near-testnet-rpc.allthatnode.com:3030',
             walletUrl: 'https://wallet.testnet.near.org',
             helperUrl: 'https://helper.testnet.near.org',
             explorerUrl: 'https://explorer.testnet.near.org',
